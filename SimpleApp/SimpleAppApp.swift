f79e81049a824615540a1645d92db37eb3aceec6
Angel Claudio <angelclaudiopr@gmail.com>
2022-08-27 10:18:39 -0400

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Angel Claudio on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
